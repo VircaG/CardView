e57878f3639535da0742c8284a1b194ae5026ccc
Virca Gonçalves <vircagoncalves39@gmail.com>
2021-05-03 18:45:17 +0100

    Linear Layout

--- app/src/main/java/com/vircagoncalves/cardview/MainActivity.java
+++ app/src/main/java/com/vircagoncalves/cardview/MainActivity.java
@@ -1,14 +1,23 @@
 package com.vircagoncalves.cardview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerPostagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerPostagem = findViewById(R.id.recyclerPostagem);
+        //Define layout
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+                recyclerPostagem.setLayoutManager(layoutManager);
+
     }
 }
